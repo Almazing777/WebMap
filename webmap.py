810eaaf5e4503b0260eab0cdf855ab68c793a5b9
Almas <mendygax@gmail.com>
2018-09-27 21:29:55 -0400

    added more functionality to the map

--- webmap.py
+++ webmap.py
@@ -4,15 +4,27 @@ import pandas
 data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", 
+    fill_color=color_producer(el), color="grey", fill_opacity=0.7))
 
-for lt, ln in zip(lat, lon):
-# for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {"fillColor":"yellow" if x["properties"]["POP2005"] < 10000000 else "orange" 
+if 1000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
 
 
 map.add_child(fg)
